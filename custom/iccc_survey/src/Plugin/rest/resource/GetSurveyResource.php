7e285b6c4a57f6e76a7de2c6e5653f723262558c
Ashley Maher <ashley.maher@didymodesigns.com.au>
2024-06-27 12:14:11 +1000

    Adding teh rest service to provide an individual surevy

---
+++ custom/iccc_survey/src/Plugin/rest/resource/GetSurveyResource.php
@@ -0,0 +1,141 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\iccc_survey\Plugin\rest\resource;
+
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+use Drupal\Core\KeyValueStore\KeyValueStoreInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\iccc_survey\Entity\Survey;
+use Drupal\rest\ModifiedResourceResponse;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Route;
+
+/**
+ * Represents Get survey resource records as resources.
+ *
+ * @RestResource (
+ *   id = "get_survey_resource",
+ *   label = @Translation("Get survey resource"),
+ *   uri_paths = {
+ *     "canonical" = "/api/get-survey-resource/{surveyId}",
+ *     "create" = "/api/get-survey-resource"
+ *   }
+ * )
+ *
+ * @DCG
+ * The plugin exposes key-value records as REST resources. In order to enable it
+ * import the resource configuration into active configuration storage. An
+ * example of such configuration can be located in the following file:
+ * core/modules/rest/config/optional/rest.resource.entity.node.yml.
+ * Alternatively, you can enable it through admin interface provider by REST UI
+ * module.
+ * @see https://www.drupal.org/project/restui
+ *
+ * @DCG
+ * Notice that this plugin does not provide any validation for the data.
+ * Consider creating custom normalizer to validate and normalize the incoming
+ * data. It can be enabled in the plugin definition as follows.
+ * @code
+ *   serialization_class = "Drupal\foo\MyDataStructure",
+ * @endcode
+ *
+ * @DCG
+ * For entities, it is recommended to use REST resource plugin provided by
+ * Drupal core.
+ * @see \Drupal\rest\Plugin\rest\resource\EntityResource
+ */
+final class GetSurveyResource extends ResourceBase {
+
+  /**
+   * The key-value storage.
+   */
+  private readonly KeyValueStoreInterface $storage;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    KeyValueFactoryInterface $keyValueFactory,
+    AccountProxyInterface $currentUser,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->storage = $keyValueFactory->get('get_survey_resource');
+    $this->currentUser = $currentUser;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new self(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('rest'),
+      $container->get('keyvalue'),
+      $container->get('current_user')
+    );
+  }
+
+
+  /**
+   * Responds to GET requests.
+   *
+   * @param string $surveyId
+   *
+   *
+ */
+  public function get($surveyId): JsonResponse
+  {
+    // You must to implement the logic of your REST Resource here.
+    // Use current user after pass authentication to validate access.
+    if (!$this->currentUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
+    }
+//    if (!$this->storage->has($surveyId)) {
+//      throw new NotFoundHttpException();
+//    }
+//    $resource = $this->storage->get($id);
+//    return new ResourceResponse($resource);
+          $survey = Survey::load($surveyId);
+      $returnValue = $survey->getJsonString();
+
+      return new JsonResponse($returnValue, 200, [], true);
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getBaseRoute($canonical_path, $method): Route {
+    $route = parent::getBaseRoute($canonical_path, $method);
+    // Set ID validation pattern.
+    if ($method !== 'POST') {
+      $route->setRequirement('id', '\d+');
+    }
+    return $route;
+  }
+
+  /**
+   * Returns next available ID.
+   */
+  private function getNextId(): int {
+    $ids = \array_keys($this->storage->getAll());
+    return count($ids) > 0 ? max($ids) + 1 : 1;
+  }
+
+}
